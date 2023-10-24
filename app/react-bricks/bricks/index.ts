fb5651af88c3ce090228fc3e8465f2ea97737dc8
Paolo Zanotti <paolo.zanotti@reactbricks.com>
2023-10-24 16:48:52 +0200

    ⬆️ Upgrade to RBv4 and add Pokemon brick

--- app/react-bricks/bricks/index.ts
+++ app/react-bricks/bricks/index.ts
@@ -1,6 +1,7 @@
 import { types } from 'react-bricks/frontend'
 import reactBricksUITheme from './react-bricks-ui/index'
 import HeroUnit from './custom/HeroUnit'
+import Pokemon from './custom/Pokemon'
 
 const bricks: types.Theme[] = [
   reactBricksUITheme, // React Bricks UI
@@ -10,6 +11,10 @@ const bricks: types.Theme[] = [
       {
         categoryName: 'Hero sections',
         bricks: [HeroUnit], // Custom Bricks
+      },
+      {
+        categoryName: 'Pokemon',
+        bricks: [Pokemon], // External data Bricks
       },
     ],
   },
